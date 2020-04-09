141c4db9f69fd09b00c652db1a2466b94155e844
Pepe Valdivia <jvaldivia@softweb.pe>
2020-04-09 16:36:42 -0500

    udpate students con nombre

--- db/fill.rb
+++ db/fill.rb
@@ -54,4 +54,36 @@ def fill_students
   end
 end
 
-fill_students
+# fill_students
+def add_name_students
+  # todos menos los de la sede central - San Isidro
+  f = File.open('./data/pepe2.txt', :encoding => 'UTF-8')
+  DB.transaction do
+    begin
+      f.each_line do |line|
+        # puts line
+        array = line.split('::')
+        student_code = array[0]
+        student_name = array[1]
+        student_lastname = array[2].strip
+        # update student
+        puts student_code
+        student = Student.where(
+          :code => student_code
+        ).first
+        if(student != nil)
+          student.name = student_lastname + ', ' + student_name
+          student.save
+        end
+      end
+      f.close
+    rescue Exception => e
+      Sequel::Rollback
+      puts 'error!'
+      puts e.message
+      puts e.backtrace
+    end
+  end
+end
+
+add_name_students
